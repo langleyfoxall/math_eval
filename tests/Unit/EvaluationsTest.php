5ca9c862c0c455d346f8975de2825701b1b9502d
Christoffer Lindahl <christoffer@trinax.se>
2022-01-25 13:23:35 +0100

    Calling "math_eval()" with invalid expression should throw an exception instead of giving PHP error.

--- tests/Unit/EvaluationsTest.php
+++ tests/Unit/EvaluationsTest.php
@@ -2,6 +2,7 @@
 
 namespace LangleyFoxall\MathEval\Tests\Unit;
 
+use LangleyFoxall\MathEval\Exception\InvalidExpression;
 use PHPUnit\Framework\TestCase;
 
 class EvaluationsTest extends TestCase
@@ -23,5 +24,11 @@ class EvaluationsTest extends TestCase
     public function testEvaluation($expression, $expectedResult)
     {
         $this->assertEquals($expectedResult, math_eval($expression));
+    }
+
+    public function testEvaluationInvalidExpressionThrows()
+    {
+        $this->expectException(InvalidExpression::class);
+        math_eval(' + ');
     }
 }
